54fcb8769f703b761733e5280e39716421bb9cb9
Ralph Schäfer <ralph.schaefer@emnify.com>
2018-03-08 08:39:25 +0100

    change startupscripts

--- client.sh
+++ client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 set -e
-
-mvn exec:java -Dexec.mainClass="test.emnify.app.akkatest" -Dakka.remote.netty.tcp.port=0
+. settings
+mvn exec:java -Dexec.mainClass="test.emnify.app.akkatest" -Dakka.remote.netty.tcp.port=0 -Dakka.cluster.seed-nodes="akka.tcp://testSystem@$SEEDIP:2551" -Dakka.remote.netty.tcp.hostname=$NODEIP
--- seed.sh
+++ seed.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 set -e
+. settings
 
-mvn exec:java -Dexec.mainClass="test.emnify.app.akkatest"  -Dexec.args="seed"
+mvn exec:java -Dexec.mainClass="test.emnify.app.akkatest"  -Dexec.args="seed" -Dakka.cluster.seed-nodes="akka.tcp://testSystem@$SEEDIP:2551" -Dakka.remote.netty.tcp.hostname=$NODEIP
