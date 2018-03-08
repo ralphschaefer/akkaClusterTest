077b67449e83b76ede5d3100d7f1011ceac69e5b
Ralph Schäfer <ralph.schaefer@emnify.com>
2018-03-08 08:51:06 +0100

    fixed ip for clustertest

--- seed.sh
+++ seed.sh
@@ -2,4 +2,4 @@
 set -e
 . settings
 
-mvn exec:java -Dexec.mainClass="test.emnify.app.akkatest"  -Dexec.args="seed" -Dakka.cluster.seed-nodes="akka.tcp://testSystem@$SEEDIP:2551" -Dakka.remote.netty.tcp.hostname=$NODEIP
+mvn exec:java -Dexec.mainClass="test.emnify.app.akkatest"  -Dexec.args="seed" -Dakka.remote.netty.tcp.hostname=$NODEIP
